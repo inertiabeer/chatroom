2a9b996e13fc734a3c0d17742cff8c295ff525ab
inertiabeer <986619667@qq.com>
2017-07-09 09:18:59 +0800

    add a load event

--- src/chatroom/Box.js
+++ src/chatroom/Box.js
@@ -1,6 +1,6 @@
-import React,{Component} from 'react';
-import Send from './Send.js';
-import Message from './Message.js';
+import React,{Component} from "react";
+import Send from "./Send.js";
+import Message from "./Message.js";
 import {Button} from "antd";
 
 class Box extends Component
@@ -9,32 +9,32 @@ class Box extends Component
 		super(props);
 		this.state={
 			listItems:[]
-		}
+		};
 
 		
 	}
 	componentDidMount() {
 		var that=this;
-		socket.on('serverMessage',function(content){
+		socket.on("serverMessage",function(content){
 
 	    let message=JSON.parse(content);
-	    that.setState({listItems:that.state.listItems.concat([<Message time={message.time} name={message.name} message={message.message}></Message>])})
+	    that.setState({listItems:that.state.listItems.concat([<Message time={message.time} name={message.name} message={message.message}></Message>])});
 
 
 
 	
-	})
+		});
 
 
 	}
 	componentDidUpdate(prevProps, prevState) {
 		
-			var node=document.getElementsByClassName("message_container");//获取整个的消息框
-			if(node[0].scrollHeight>=node[0].clientHeight)
-			{
-				node[0].scrollIntoView(false);
-				node[0].scrollTop=node[0].scrollHeight
-			}
+		var node=document.getElementsByClassName("message_container");//获取整个的消息框
+		if(node[0].scrollHeight>=node[0].clientHeight)
+		{
+			node[0].scrollIntoView(false);
+			node[0].scrollTop=node[0].scrollHeight;
+		}
 	}
 	componentWillUpdate(nextProps, nextState) {
 		
@@ -43,8 +43,8 @@ class Box extends Component
 	{
 		return (
 			<div className='message_box'>
-			<div className='message_container'>{this.state.listItems}</div>
-			<Send></Send>
+				<div className='message_container'>{this.state.listItems}</div>
+				<Send></Send>
 				
 			</div>);
 	}
